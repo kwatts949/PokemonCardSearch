6e50d94d5c73ea9c543569ea8819cad546bbb7c2
Kayleigh Watts <kwatts949@gmail.com>
2023-01-24 10:27:40 +0000

    Add buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ const Navbar = () => {
     <header>
       <div className="navbar">
         <h1>Pokemon Card Search App</h1>
+        <input type="text" placeholder="Please enter a card name"/>
+        <button>Search for cards!</button>
       </div>
     </header>
   );
