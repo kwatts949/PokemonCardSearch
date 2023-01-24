aadd070b0cb53f4baa61a556aa886d5cd01a959a
Kayleigh Watts <kwatts949@gmail.com>
2023-01-24 10:02:55 +0000

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <header>
+      <div className="navbar">
+        <h1>Pokemon Card Search App</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
